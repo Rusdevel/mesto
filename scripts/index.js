79f96a751f3ee9ec7bda5d805b0589167da27e6c
Ruslan <ruslanbestaev77@yandex.ru>
2021-04-01 12:54:54 +0300

    Исправил js, исправил профайл и попап

--- scripts/index.js
+++ scripts/index.js
@@ -1,58 +1,36 @@
+const profileButton = document.querySelector('.profile__button'); 
+const popup = document.querySelector('.popup'); 
+const closeButton = document.querySelector('.popup__close'); 
+const profileName = document.querySelector('.profile__name'); 
+const profileJob = document.querySelector('.profile__job');
+// Находим форму
+const formElement = document.querySelector('.popup__form'); 
+    // Находим поля формы в DOM 
+let nameInput = formElement.querySelector('.popup__input_type_name'); 
+let jobInput = formElement.querySelector('.popup__input_type_job'); 
 
-let profileButton = document.querySelector('.profile__button');
-let popup = document.querySelector('.popup');
-let closeButton = document.querySelector('.popup__close');
-/*
-let profileName = document.querySelector('.profile__name');
-let job = document.querySelector('.profile__job');
-let profileInfo = document.querySelector('.profile__info');
-let saveButton = document.querySelector('.popup__button');
-*/
-let formElement = document.querySelector('.popup__form'); // Воспользуйтесь методом querySelector()
-    // Находим поля формы в DOM
-let nameInput = formElement.querySelector('.popup__input_type_name');
-let jobInput = formElement.querySelector('.popup__input_type_job');
-
-function formSubmitHandler(evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
-    // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-    let nameValue = nameInput.value;
-    let jobValue = jobInput.value;
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let profileName = document.querySelector('.profile__name');
-    let profileJob = document.querySelector('.profile__job');
-
-    // Вставьте новые значения с помощью textContent
-    profileName.textContent = nameValue;
-    profileJob.textContent = jobValue;
-}
-
-formElement.addEventListener('submit', formSubmitHandler);
-
-
-function openPopup () {
-    popup.classList.remove('popup_open');
-}
-
-function closePopup() {
-    popup.classList.add('popup_open');
-}
-
-profileButton.addEventListener('click', openPopup);
-
+ // Функция отправки формы
+function formSubmitHandler(evt) { 
+    evt.preventDefault();  
+    profileName.textContent = nameInput.value; 
+    profileJob.textContent = jobInput.value;
+// Закрываем попап посли отправки формы
+    closePopup();
+} 
+ // Обработчик события на отправку формы
+formElement.addEventListener('submit', formSubmitHandler); 
+ 
+ // Функия открытия попапа
+function openPopup () { 
+    popup.classList.add('popup_open'); 
+    nameInput.value = profileName.textContent; 
+    jobInput.value = profileJob.textContent;
+} 
+ // Функия закрытия попапа
+function closePopup() { 
+    popup.classList.remove('popup_open'); 
+} 
+ // Обработчик события на открытие попапа
+profileButton.addEventListener('click', openPopup); 
+ // Обработчик события на закрытие попапа
 closeButton.addEventListener('click', closePopup);
-
-
-/*
-function addProfileDate () {
-    profileInfo.insertAdjacentHTML('beforeend', `<h1 class="profile__name">${profileName}</h1>
-                    <button type="button" class="button profile__button"></button>
-                    <p class="profile__job">${job}</p>`)
-}
-*/
-
-
